c365e955246d6d75c3f5877884996ede047ebc66
Colm Doyle <cdoyle@slack-corp.com>
2021-09-08 12:43:51 +0100

    update index.js to use bolt

--- index.js
+++ index.js
@@ -1,100 +1,38 @@
-/* ***************************************************
- * Reacilator for Slack
- * Translate message when a user reacted with an emoji
- * Tomomi Imura (@girlie_mac)
- * ****************************************************/
+require('dotenv').config();
 
- /* Slack App setup
-  * Enable events: "reaction_added"
-  * Enable Bot user
-  * Scopes: "chat:write:bot" (Send messages with chat.postMessage by a bot),
-  *         "reactions:read" (Access the workspace’s emoji reaction history)
-  *         "*:read" (Access channels info)
-  *         "*:history" (Access channels history)
-  */
+const langcode = require('./langcode');
 
- /* Google Cloud setup
-  * API Key https://cloud.google.com/translate/docs/getting-started
-  * Node Lib https://www.npmjs.com/package/@google-cloud/translate
-  */
+const { App } = require('@slack/bolt');
+const { Translate } = require('@google-cloud/translate').v2;
 
- const express = require('express');
- const bodyParser = require('body-parser');
- const signature = require('./verifySignature');
- const langcode = require('./langcode');
- const axios = require('axios'); 
- const qs = require('qs');
- 
- const apiUrl = 'https://slack.com/api';
- 
- const app = express();
-
-/*
- * Parse application/x-www-form-urlencoded && application/json
- * Use body-parser's `verify` callback to export a parsed raw body
- * that you need to use to verify the signature
- */
-
-const rawBodyBuffer = (req, res, buf, encoding) => {
-  if (buf && buf.length) {
-    req.rawBody = buf.toString(encoding || 'utf8');
-  }
-};
-
-app.use(bodyParser.urlencoded({verify: rawBodyBuffer, extended: true }));
-app.use(bodyParser.json({ verify: rawBodyBuffer }));
-
-
-const googleCredentials = {
-  projectId: process.env.GOOGLE_PROJECT_ID,
-  key: process.env.GOOGLE_KEY
-};
-
-app.post('/events', (req, res) => {
-  switch (req.body.type) {
-    case 'url_verification': {
-      // verify Events API endpoint by returning challenge if present
-      res.send({ challenge: req.body.challenge });
-      break;
-    }
-    case 'event_callback': {
-      // Verify the signing secret
-      if (!signature.isVerified(req)) {
-        res.sendStatus(404);
-        return;
-      } else {
-        res.sendStatus(200);
-        return events(req, res);
-      }
-      break;
-    }
-    default: { res.sendStatus(404); }
-  }
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
 });
 
-// Require Google Cloud Translation API 
-const translate = require('@google-cloud/translate')(googleCredentials);
+const translate = new Translate({
+  projectId: process.env.GOOGLE_PROJECT_ID,
+});
 
-/* Events */
-
-const events = async(req, res) => {
-  const {type, user, reaction, item} = req.body.event;
+app.event('reaction_added', async ({ event, client }) => {
+  const { type, reaction, item } = event;
 
   if (type === 'reaction_added') {
     // If reacji was triggered && it is a correct emoji, translate the message into a specified language
 
-    if(item.type !== 'message') {
+    if (item.type !== 'message') {
       return;
     }
 
     let country = '';
 
     // Check emoji if it is a country flag
-    if(reaction.match(/flag-/)) { // when an emoji has flag- prefix
+    if (reaction.match(/flag-/)) { // when an emoji has flag- prefix
       country = reaction.match(/(?!flag-\b)\b\w+/)[0];
     } else { // jp, fr, etc.
       const flags = Object.keys(langcode); // array
-      if(flags.includes(reaction)) {
+      if (flags.includes(reaction)) {
         country = reaction;
       } else {
         return;
@@ -104,52 +42,48 @@ const events = async(req, res) => {
     // Finding a lang based on a country is not the best way but oh well
     // Matching ISO 639-1 language code
     let lang = langcode[country];
-    if(!lang) return;
+    if (!lang) return;
 
-    let messages = await getMessage(item.channel, item.ts); 
-    postTranslatedMessage(messages, lang, item.channel, reaction);
-    
+    let messages = await getMessage(item.channel, item.ts, client);
+    postTranslatedMessage(messages, lang, item.channel, reaction, client);
+
   }
-};
+});
 
-const getMessage = async(channel, ts) => { 
-  const args = {
-    token: process.env.SLACK_ACCESS_TOKEN,
-    channel: channel,
-    ts: ts,
-    limit: 1,
-    inclusive: true
-  };
-  
-  const result = await axios.post(`${apiUrl}/conversations.replies`, qs.stringify(args));
-  
+const getMessage = async (channel, ts, client) => {
   try {
-    return result.data.messages; 
-  } catch(e) {
+    const result = await client.conversations.replies({
+      channel: channel,
+      ts: ts,
+      limit: 1,
+      inclusive: true
+    });
+    return result.messages;
+  } catch (e) {
     console.log(e);
   }
 };
 
-const postTranslatedMessage = (messages, lang, channel, emoji) => {
-  
+const postTranslatedMessage = (messages, lang, channel, emoji, client) => {
+
   // Google Translate API
-  
+
   let message = messages[0];
   translate.translate(message.text, lang, (err, translation) => {
     if (err) {
       console.log(err);
     } else {
-      if(isAlreadyPosted(messages, translation)) return;
-      postMessage(message, translation, lang, channel, emoji);
+      if (isAlreadyPosted(messages, translation)) return;
+      postMessage(message, translation, lang, channel, emoji, client);
     }
   });
 };
 
 const isAlreadyPosted = (messages, translation) => {
-  // To avoid posting same messages several times, make sure if a same message in the thread doesn't exist
+  // To avoid posting same messages several times, check the thread for an identical translation
   let alreadyPosted = false;
   messages.forEach(messageInTheThread => {
-    if (!alreadyPosted && messageInTheThread.subtype && messageInTheThread.attachments[0].text === translation) {
+    if (!alreadyPosted && messageInTheThread.subtype && messageInTheThread.blocks[0].text.text === translation) {
       alreadyPosted = true;
     }
   });
@@ -158,53 +92,67 @@ const isAlreadyPosted = (messages, translation) => {
   }
 };
 
+const postMessage = async (message, translation, lang, channel, emoji, client) => {
 
-// Bot posts a message 
-const postMessage = async(message, translation, lang, channel, emoji) => { 
-  
-  let ts = (message.thread_ts) ? message.thread_ts : message.ts;
+  const ts = (message.thread_ts) ? message.thread_ts : message.ts;
 
-  // TODO - Once Block Kit supports the "attachment" bar, switch this part to Block Kit!
-  
-  let attachments = [];
-  if(message.text) {
-    attachments = [
+  let text = '';
+  let blocks = [];
+
+  if (message.text) { // Check if the message has translated
+    text = `_Here is a translation to_ :${emoji}: _(${lang})_`;
+    blocks.push(
       {
-        pretext: `_The message is translated in_ :${emoji}: _(${lang})_`,
-        text: translation,
-        footer: message.text,
-        mrkdwn_in: ["text", "pretext"]
-      }
-    ];
+        type: "section", 
+        text: {
+          type: "mrkdwn", 
+          text: `${translation}` 
+        }
+      },
+      {
+        type: "context", 
+        elements: [
+          { type: "mrkdwn", text: `A translation of the original message to :${emoji}: _(${lang})_` }
+        ] 
+      },
+    );
   } else {
-    attachments = [
+    text = '_Sorry, the language is not supported!_ :persevere:';
+    blocks.push(
       {
-        pretext: '_Sorry, the language is not supported!_ :persevere:',
-        mrkdwn_in: ["text", "pretext"]
+        type: "section", 
+        text: {
+          type: "mrkdwn", 
+          text: `_Sorry, the language is not supported!_ :persevere:` 
+        }
       }
-    ];
+    );
   }
-  
-  const args = {
-    token: process.env.SLACK_ACCESS_TOKEN,
-    channel: channel,
-    attachments: JSON.stringify(attachments),
-    as_user: false,
-    username: 'Reacjilator Bot',
-    thread_ts: ts
-  };
-  
-  const result = await axios.post(`${apiUrl}/chat.postMessage`, qs.stringify(args));
-  
+
   try {
-    console.log(result.data);
-  } catch(e) {
+    const result = await client.chat.postMessage({
+      text,
+      blocks,
+      channel,
+      thread_ts: ts
+    });
+
+    console.log(result);
+  } catch (e) {
     console.log(e);
   }
 };
 
 
-
-const server = app.listen(process.env.PORT || 5000, () => {
-  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
-});
+(async () => {
+  try {
+    // Start your app
+    await app.start();
+    // eslint-disable-next-line no-console
+    console.log('⚡️ Bolt app is running!');
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Unable to start App', error);
+    process.exit(1);
+  }
+})();
